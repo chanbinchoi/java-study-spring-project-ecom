2fca8d7c3e90c995c12822b03a2d8c481d4029a2
chanbinchoi <classy9611@gmail.com>
2026-03-23 23:04:04 +0900

    Fetch Product By Id

---
+++ src/main/java/com/telusko/SpringEcom/repo/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.telusko.SpringEcom.repo;
+
+import com.telusko.SpringEcom.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
---
+++ src/main/java/com/telusko/SpringEcom/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.telusko.SpringEcom.service;
+
+import com.telusko.SpringEcom.model.Product;
+import com.telusko.SpringEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(null);
+    }
+}
