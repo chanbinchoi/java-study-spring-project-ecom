3d4d939a73b172ab996cceb51376292826845502
chanbinchoi <classy9611@gmail.com>
2026-03-24 23:35:40 +0900

    search

--- src/main/java/com/telusko/SpringEcom/service/ProductService.java
+++ src/main/java/com/telusko/SpringEcom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.telusko.SpringEcom.model.Product;
 import com.telusko.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,5 +21,22 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return productRepo.findById(id).orElse(null);
+    }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
     }
 }
